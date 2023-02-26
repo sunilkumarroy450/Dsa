31d4fd2c4bb4cbbc79819923d25deac320258dbe
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-02-26 22:43:33 +0530

    bubble sort problem

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 // #Slection Sort;
 
+// function Sort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     min = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[min]) {
+//         min = j;
+//       }
+//     }
+//     swap(arr, min, i);
+//   }
+//   return arr;
+// }
+
+// function swap(arr, i, j) {
+//   temp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = temp;
+// }
+
+// let arr = [3, 5, 0, 9, 8];
+// console.log(Sort(arr));
+
+// #Bubble Sort
 function Sort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    min = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[min]) {
-        min = j;
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
       }
     }
-    swap(arr, min, i);
   }
   return arr;
 }
