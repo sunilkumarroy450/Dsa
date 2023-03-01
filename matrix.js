b781819bc85996575798b39d26f6e765fc5a216d
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-03-01 20:30:45 +0530

    Spiral Traversal, Rectangular Matrix

--- matrix.js
+++ matrix.js
@@ -23,6 +23,7 @@ let mat = [
 ];
 nTraversal(mat);
 
+//##Reverse N Traversal
 function reverseNtraversal(matrix) {
   var bag = "";
   for (var i = 0; i < matrix.length; i++) {
@@ -45,3 +46,49 @@ let mat1 = [
   [7, 8, 9],
 ];
 reverseNtraversal(mat1);
+
+//##Spiral Traversal (Rectangular Matrix)
+
+function SpiralTraversal(arr, N, M) {
+  top = 0;
+  left = 0;
+  bottom = N - 1;
+  right = M - 1;
+  size = N * M;
+  ans = [];
+  while (ans.length < size) {
+    for (i = top; i <= bottom && ans.length < size; i++) {
+      //traversing the left most unvisited column from top to botoom
+      ans.push(arr[i][left]);
+    }
+    left++; //incrementing the left boundary
+
+    for (i = left; i <= right && ans.length < size; i++) {
+      //traversing the bottom most unvisited row from left to right
+      ans.push(arr[bottom][i]);
+    }
+    bottom--; //decrementing the bottom boundary
+
+    for (i = bottom; i >= top && ans.length < size; i--) {
+      //traversing the right most unvisited column from bottom to top
+      ans.push(arr[i][right]);
+    }
+    right--; //decrementing the right boundary
+
+    for (i = right; i >= left && ans.length < size; i--) {
+      //traversing the top most unvisited row from right to left
+      ans.push(arr[top][i]);
+    }
+    top++; //incrementing the top boundary
+  }
+  console.log(ans.join(" "));
+}
+
+let mat2 = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+];
+let N = 3;
+let M = 4;
+SpiralTraversal(mat2, N, M);
